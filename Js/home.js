0dd00518d5f089d6d6a108d1e12ee3f2cf977b8a
Or28mahmoud <85789223+Or28mahmoud@users.noreply.github.com>
2023-06-23 16:59:00 +0200

    ich habe eine file für javascript erstellt.

---
+++ Js/home.js
@@ -0,0 +1,10 @@
+let one =document.getElementById("li_1");
+let two =document.getElementById("li_2");
+let  three =document.getElementById("li_3");
+let four =document.getElementById("li_4");
+let five =document.getElementById("li_5");
+one.onclick =function(){}
+two.onclick =function(){}
+three.onclick =function(){}
+four.onclick =function(){}
+five.onclick =function(){}
